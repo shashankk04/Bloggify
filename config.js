a66fb6f1aac4898d69399c447bc918852653d189
Shashank <143308371+shashankk04@users.noreply.github.com>
2024-01-04 21:50:52 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://shiv04313:RAJgGFA2KzuSUSlZ@bloggify.y3yzudo.mongodb.net/?retryWrites=true&w=majority"
-    // Add other configuration settings if needed
-  };
