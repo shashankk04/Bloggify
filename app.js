7c34e0341d1f8dafa0fd064b8e491e16e7dbf6c2
Shashank <shiv04313@gmail.com>
2024-01-04 20:57:41 +0530

    Connected to atlas

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view engine","ejs");
-//Connecting to mongodb 
+app.set("view engine", "ejs");
+
+// Connecting to MongoDB Atlas
 mongoose.connect(
-    "mongodb://localhost/blog",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
-//routes
+  "mongodb+srv://shiv04313:p35htOeLIFDIqDxN@bloggify.wid7sej.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
+
+// Routes
 app.use(require("./routes/index"));
 app.use(require("./routes/blog"));
 app.use(require("./routes/compose"));
 
-
-app.listen(3000,()=>{
-    console.log("App listening on port 3000");
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
 });
